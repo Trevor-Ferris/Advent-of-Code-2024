9a126c631bcd245722b7f060509e4a5dd14b443e
Trevor Ferris <Trevor.f@comcast.net>
2025-02-06 19:40:00 -0800

    changed to object wires

--- Aoc24/Aoc24p1.py
+++ Aoc24/Aoc24p1.py
@@ -3,6 +3,7 @@ Advent of Code day 24 part 1
 Written by Trevor Ferris
 2/6/2024
 Notes: check that sick list comprehension
+changed to object based wires
 """
 
 """            Part 1           
@@ -20,47 +21,104 @@ because each wire can only recieve input from 1 gate
 dict of wires with values, list of gates 
 run down list of gates removing any gates which have both values and add the resulting wire to the dict of wires until the list is empty"""
 
+"""            Part 2
+System is trying to binary add x wires with y wires resulting in a z wire equal to x + y
+System should be taking in two n bit sets of wires and resulting in n + 1 bits on the z wire
+4 pairs of gates are swapped 
+record the output wire that is involved in the swap
+find which gates on the output are incorrect
+(from known answer XOR initial result)
+find which gates control those results and swap them
+build reverse dict of wires : gates instr
+17 bits have the wrong output but only 8 pairs of gates can be swapped
+initial wires cant be swapped
+normal bitwise add is like 1 XOR 1 s and an AND for the carry?
+for a swap to effect multiple z gates it must be effecting multiple outputs
+
+"""
 from enum import StrEnum
 
 GATES_FILENAME = "Aoc24/input.txt"
 
+class Wire(object):
+    def __init__(self, val, from_gate, to_gates):
+        self.val = val
+        self.from_gate = from_gate
+        self.to_gates = to_gates
+
+    def update(self, val, from_gate, to_gates):
+        if val != None:
+            self.val = val
+        if from_gate != None:
+            self.from_gate = from_gate
+        if to_gates != None:
+            self.to_gates = to_gates
+            
+    def get_val(self):
+        return self.val
+    
+    def read_instr(self, g1, g2, instr):
+        match instr:
+            case "OR":
+                return g1 | g2 
+            case "AND":
+                return g1 & g2
+            case "XOR":
+                return g1 ^ g2
+        
+    def find_val(self, gates):
+        if self.val == None:
+            if (x := gates[self.from_gate[0]].get_val()) != None and (y := gates[self.from_gate[1]].get_val()) != None:
+                self.val = self.read_instr(int(x), int(y), self.from_gate[2])
+                return True
+            return False
+        return True
+    
 def load_gates(file_name):
     gates = {}
-    instructions = []
     with open(file_name, "r") as gates_file:
         for line in gates_file:
             if line == ("\n"):
                 break
             line = line.rstrip("\n")
             gate, val = line.split(":")
-            gates[gate] = int(val)
+            gates[gate] = Wire(int(val), None, None)
         for line in gates_file:
             line = line.rstrip("\n")
             g1, instr, g2, arrow, g3 = line.split()
-            instruction = (g1, g2, instr, g3)
-            instructions.append(instruction)
-    return gates, instructions
+            if g1 in gates:
+                gates[g1].update(None, None, (g1, g2, instr))
+            else:
+                gates[g1] = Wire(None, None, (g1, g2, instr))
+            if g2 in gates:
+                gates[g2].update(None,None, (g1, g2, instr))
+            else:
+                gates[g2] = Wire(None, None, (g1, g2, instr))
+            if g3 in gates:
+                gates[g3].update(None, (g1, g2, instr), None)
+            else:
+                gates[g3] = Wire(None, (g1, g2, instr), None)
+    return gates
 
-def read_instr(g1, g2, instr):
-    match instr:
-        case "OR":
-            return g1 | g2 
-        case "AND":
-            return g1 & g2
-        case "XOR":
-            return g1 ^ g2
+def dict_gates_to_binary(wires, wire_type):
+    return int("".join([str((wires[i].get_val())) for i in reversed(sorted(wires)) if wire_type in i]), 2)
         
-def calc_gates(gates, instructions):
-    while instructions:
-        for g1, g2, instr, g3 in instructions.copy():
-            if g1 in gates and g2 in gates:
-                gates[g3] = read_instr(gates[g1], gates[g2], instr)
-                instructions.remove((g1, g2, instr, g3))
-    return int("".join([str(gates[i]) for i in reversed(sorted(gates)) if "z" in i]), 2)
+def calc_gates(gates): 
+    while True:
+        comp = True
+        for x in gates.values():
+            if not x.find_val(gates):
+                comp = False
+        if comp == True:
+            return
 
 def main():
-    gates, instructions = load_gates(GATES_FILENAME)
-    print(f"Result of Z gates: {calc_gates(gates, instructions)}")
+    gates = load_gates(GATES_FILENAME)
+    calc_gates(gates)
+    bin_x = dict_gates_to_binary(gates, "x")
+    bin_y = dict_gates_to_binary(gates, "y")
+    bin_z = dict_gates_to_binary(gates, "z")
+    print(bin_x, bin_y, bin_z)
 
 if __name__ == ("__main__"):
     main()
